5a7f6515723ebb5f54362b49bab05aeb0995c02c
David Angelo <s523035@nwmissouri.edu>
2022-04-06 17:38:04 -0500

    add selector endpoints

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var glintstoneRouter = require('./routes/glintstone');
 var usersRouter = require('./routes/users');
 var addmodsRouter = require('./routes/addmods');
+var selectorRouter = require('./routes/selector');
 
 
 var app = express();
@@ -26,6 +27,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/glintstone', glintstoneRouter);
 app.use('/addmods', addmodsRouter);
+app.use('/selector', selectorRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
